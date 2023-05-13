afbfe4ce857a9c94eb094c0536d1534d7cc9d15d
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-05-13 14:28:46 +0200

    modified pipeline

--- pipeline.py
+++ pipeline.py
@@ -6,28 +6,42 @@ import sys
 sys.path.append(r'..\\helpers')
 
 from loguru import logger
+from helpers.model import DenseNetModel
 
 class BreaKHisPipeline:
     def __init__(
             self, 
-            n_epochs =  25,
+            num_epochs =  25,
             learning_rate = 0.001,
             batch_size = 8,
             data_split_train_ratio = 0.6,
-            image_size = (224, 224, 3)
+            image_size = (224, 224, 3),
+            num_classes=8
             ):
-        self.n_epochs = n_epochs
-        self.learning_rate = learning_rate
-        self.batch_size = batch_size
+        
+        self.n_epochs               = num_epochs
+        self.num_classes            = num_classes 
+        self.learning_rate          = learning_rate
+        self.batch_size             = batch_size
         self.data_split_train_ratio = data_split_train_ratio
-        self.data_split_test_ratio = round(1 - data_split_train_ratio, 2)
-        self.image_size = image_size
+        self.data_split_test_ratio  = round(1 - data_split_train_ratio, 2)
+        self.image_size             = image_size
 
         
         logger.info(f"Class Initialized: {self.__dict__}")
 
-    def fit():
-        pass
+    def fit(self):
+
+        # Get the model
+        model_object = DenseNetModel(
+            num_classes = self.num_classes,
+            input_shape = self.image_size
+        )
+
+        model = model_object.model()
+        model.summary()
 
 
 pipeline = BreaKHisPipeline()
+
+pipeline.fit()
