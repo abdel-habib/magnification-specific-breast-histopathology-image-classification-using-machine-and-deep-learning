8e9aa12df9b1f9a2922b1330eb751d4713d5b5eb
Edwing Ulin <eulinbriseno@gmail.com>
2023-05-18 12:20:24 +0200

    Split data with python

--- helpers/data_split.py
+++ helpers/data_split.py
@@ -1,43 +1,101 @@
 import os
-import tensorflow as tf
-
+import random
+import shutil
 from loguru import logger
 
 class DataSplit:
     def __init__(self):
-      self.dataset = None
-      self.left_size = None
-      self.rigth_size = None
-      self.shuffle=False
-      self.seed = None
-      logger.info(f"Class initialized: {self.__dict__}")
+      self.src_dir = os.getcwd()
+      self.parentfolder = ['100X','200X','400X','40X']
+      self.subfolders = ['papillary_carcinoma', 'ductal_carcinoma', 'mucinous_carcinoma', 'lobular_carcinoma', 'adenosis', 'tubular_adenoma', 'fibroadenoma', 'phyllodes_tumor']
+      self.subsubfolders = ['test','validation','testing']
+      self.folderName = 'BreakHis'
       
-    def get_division(self):
-        test_data,training_data = tf.keras.utils.split_dataset(
-            dataset=self.dataset,
-            left_size= self.left_size,
-            shuffle=self.shuffle,
-            seed=self.seed
-        )
-        return test_data,training_data
+    def get_files_for_magnification(self,home_dir,magnification):
+        src_dir = home_dir = os.path.join(os.getcwd(),home_dir)
+        result = []
+        folder = []
+        for root, dirs, files in os.walk(src_dir):
+            for directory in dirs:
+                if directory.endswith(magnification):
+                    folder_path = os.path.join(root, directory)
+                    folder.append(directory)
+                    for file_name in os.listdir(folder_path):
+                        file_path = os.path.join(folder_path, file_name)
+                        result.append(file_path)        
+        return result, folder
     
-    def set_on_shuffle(self,seed):
-        self.shuffle = True
-        self.seed = seed
-        return None
+    def search_label_in_array(self,dir_array,label):
+        list = []
+        for i in range(0,len(dir_array)):
+            if dir_array[i].__contains__(label):
+                list.append(dir_array[i])
+        return list
+
+    def split_directories(self,dir_list,magnification,label):
+        print('  ')
+        print()
+        
+        random.shuffle(dir_list)
+        random.shuffle(dir_list)
+        random.shuffle(dir_list)
     
-    def set_off_shuffle(self):
-        self.shuffle=False
-        self.seed = None
-        return None
+        len_50 = round(len(dir_list)*0.5)
+        len_30 = round(len(dir_list)*0.30)
     
-    def set_dataset(self,dataset):
-        self.dataset = dataset
-        return None
+        train = dir_list[0:len_50]
+        test  = dir_list[len_50:len_50+len_30]
+        validation = dir_list[len_50+len_30:len(dir_list)]
+        
+        print('Length of {}_{}: {}'.format(label,magnification,len(dir_list)))
+        print('Length of train: {}'.format(len(train)))
+        print('Length of validation: {}'.format(len(validation)))
+        print('Length of test: {}'.format(len(test)))
     
-    def set_splitingratio(self,ratio):
-        self.left_size = ratio
-        self.rigth_size = 1 - ratio
+        return train, validation, test 
+    
+    def make_files(self):
+
+        
+        above_path = os.path.join(self.src_dir,'..',self.folderName)
+        os.mkdir(above_path)
+        
+        for element in self.parentfolder:
+            path = os.path.join(above_path,element)
+            os.mkdir(path)
+            for element in self.subfolders:
+                sub_folder_path=os.path.join(path,element)
+                os.mkdir(sub_folder_path)
+                for element in self.subsubfolders:
+                    subsub_folder_path = os.path.join(sub_folder_path,element)
+                    os.mkdir(subsub_folder_path)
+    
+    def save_images_in_folder(self,data,folder_name,label,magnification):
+        dest_dir = os.path.join(self.src_dir,'..',self.folderName,magnification,label,folder_name)
+        for elements in data:
+            shutil.copy(elements,dest_dir)
+        
+                    
+    def split_data(self):
+        home_dir = os.path.join(self.src_dir, os.path.dirname("../BreaKHis_v1/histology_slides/breast/"))
+        for magnifications in self.parentfolder:
+            for label in self.subfolders:
+                result,folder = self.get_files_for_magnification(home_dir,magnifications)
+                labels_array = self.search_label_in_array(result,label)
+                train, validation, test = self.split_directories(labels_array,magnifications,label)
+                self.save_images_in_folder(train,'train',label,magnifications)
+                self.save_images_in_folder(validation,'validation',label,magnifications)
+                self.save_images_in_folder(test,'test',label,magnifications)
+
+split = DataSplit()
+
+#uncommet for creating folder strcuture once
+split.make_files()
+
+split.split_data()                        
+    
+    
+    
 
 
 
